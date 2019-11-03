3b66216a9741e9701c4ad29a4eab5ad380a95d43
Gabriel DH <wackoliberal@gmail.com>
2019-11-03 17:01:58 -0500

    the previous merge kinda fucked my shit up but hopefully this time around we're all good

---
+++ newCase/cardClasses.js
@@ -0,0 +1,211 @@
+class Card {
+  constructor(numberOfLines,lineEndings){
+    this.numberOfLines = numberOfLines; //How many lines on the card
+    this.lineEndings = lineEndings; //This is the permutation [a,b,c,...] of the line
+
+    //see if we are trying to use the card or not, and where it's living
+    this.activated = false;
+    this.onPosition = null;
+    this.offPosition = null;
+
+    //if the card is activated we remember the colors on the left and right
+    this.leftColors = [];
+    this.rightColors = [];
+    for(var i=0;i<numberOfLines;i++){
+      this.leftColors.push(null);
+      this.rightColors.push(null);
+    }
+
+    this.cardBack = two.makeRoundedRectangle(0,0,cardWidth,cardHeight,15);
+    this.cardBack.fill = 'lightgrey';
+    this.cardBack.stroke = 'black';
+
+    this.center = [0,0];
+
+    this.blackPaths = [];
+    this.colorPaths = [];
+
+    //create all the paths
+    for(var i=0;i<numberOfLines;i++){
+      var newBlackPath = two.makePath(xList[0],yList[i],xList[1],yList[i],xList[2],yList[this.lineEndings[i]],xList[3],yList[this.lineEndings[i]],open = true);
+      newBlackPath.stroke = 'black';
+      newBlackPath.fill = 'none';
+      newBlackPath.linewidth = 2;
+
+      var newColorPath = two.makePath(xList[0],yList[i],xList[1],yList[i],xList[2],yList[this.lineEndings[i]],xList[3],yList[this.lineEndings[i]],open = true);
+      newColorPath.stroke = 'none';
+      newColorPath.fill = 'none';
+      newColorPath.linewidth = 3;
+      newColorPath.dashes[0] = 300;
+      newColorPath.dashes.offset = 300;
+
+      this.blackPaths.push(newBlackPath);
+      this.colorPaths.push(newColorPath);
+    }
+
+    this.outerCircle = two.makeCircle(this.center[0],yList[this.numberOfLines],10);
+    this.outerCircle.fill = 'none';
+    this.outerCircle.stroke = 'black';
+
+    this.innerCircle = two.makeCircle(this.center[0],yList[this.numberOfLines],5);
+    this.innerCircle.fill = 'none';
+    this.innerCircle.stroke = 'none';
+
+    //Next we must figure out if we're an inversion.
+    var inversionCount = 0;
+    for(var i=0;i<this.numberOfLines;i++){
+      for(var j=i;j<this.numberOfLines;j++){
+        if(this.lineEndings[j]<this.lineEndings[i]){
+          inversionCount++;
+        }
+      }
+    }
+    if(inversionCount%2==1){
+      this.innerCircle.fill = 'black';
+    }
+
+    this.card = two.makeGroup(this.cardBack, this.innerCircle, this.outerCircle);
+    for(var i=0;i<this.numberOfLines;i++){
+      this.card.add(this.blackPaths[i]);
+      this.card.add(this.colorPaths[i]);
+    }
+    this.card.translation.set(-100,-100);
+    this.center = [-100,-100];
+  }
+  moveTo(position,timeLimit = .2){
+    var moveX = position[0] - this.center[0];
+    var moveY = position[1] - this.center[1];
+    var stepLimit = 60 * timeLimit;
+    var stepX = moveX/stepLimit;
+    var stepY = moveY/stepLimit;
+    var elapsed = 0;
+    var animationData = [animateMove,[this,stepX,stepY,elapsed,stepLimit,this.center]];
+    animationQueue.push(animationData);
+    this.center = position;
+  }
+  colorize(){
+    //First figure out what color each line is
+    if(this.onPosition==0){
+      for(var i=0;i<this.numberOfLines;i++){
+        this.leftColors[i] = colors[i];
+      }
+    }
+    else{
+      for(var i=0;i<this.numberOfLines;i++){
+        this.leftColors[i] = activeSlots[this.onPosition-1].rightColors[i];
+      }
+    }
+    //Then set the colors of the line and associated lines and balls
+    for(var i=0;i<this.numberOfLines;i++){
+      this.colorPaths[i].stroke = this.leftColors[i];
+      this.rightColors[this.lineEndings[i]] = this.leftColors[i];
+      powerBalls[2*this.onPosition+1][i].fill = this.leftColors[i];
+      powerBalls[2*this.onPosition+2][this.lineEndings[i]].fill = this.leftColors[i];
+      powerLines[this.onPosition][i].stroke = this.leftColors[i];
+    }
+
+    var leftLineData = [growLines,this.onPosition];
+    animationQueue.push(leftLineData);
+    var leftBallData = [growBalls,2*this.onPosition+1];
+    animationQueue.push(leftBallData);
+    var animationData = [animateColors,this];
+    animationQueue.push(animationData);
+    var rightBallData = [growBalls,2*this.onPosition+2];
+    animationQueue.push(rightBallData);
+  }
+  decolorize(){
+    for(var i=0;i<this.numberOfLines;i++){
+      this.colorPaths[i].stroke = 'none';
+      this.colorPaths[i].dashes.offset = 300;
+      this.leftColors[i] = null;
+      this.rightColors[i] = null;
+    }
+    //decolorize the balls and lines too!
+    //NOTE: 7 and 16 should be replaced by cardsInPlay and 2*(cardsInPlay+1)
+    for(var i = this.onPosition;i<numberOfSlots + 1;i++){
+      for(var j=0;j<this.numberOfLines;j++){
+        powerLines[i][j].dashes.offset=50;
+        powerLines[i][j].stroke = 'none'
+      }
+    }
+    for(var i=2*this.onPosition+1;i<2*numberOfSlots+2;i++){
+      for(var j=0;j<this.numberOfLines;j++){
+        powerBalls[i][j].radius = 0;
+        powerBalls[i][j].fill = 'none';
+      }
+    }
+  }
+  activate(){
+    this.activated = true;
+    this.onPosition = activeSlots.length
+    this.moveTo(activePositions[this.onPosition]);
+    activeSlots.push(this);
+    inactiveSlots[this.offPosition] = null;
+    this.colorize();
+  }
+  deactivate(){
+    this.activated = false;
+    inactiveSlots[this.offPosition] = this;
+    this.decolorize();
+    this.moveTo(inactivePositions[this.offPosition]);
+  }
+  onClick(){
+    if(this.activated){
+      this.deactivate();
+      return true;
+    }
+    else{
+      this.activate();
+      return false;
+    }
+  }
+  destroy(){
+    this.cardBack.fill = 'none';
+    this.cardBack.stroke = 'none';
+    this.innerCircle.fill = 'none';
+    this.outerCircle.stroke = 'none';
+    for(var i=0;i<this.numberOfLines;i++){
+      this.blackPaths[i].stroke = 'none';
+      this.colorPaths[i].stroke = 'none';
+    }
+    this.card.translation.set(-100,-100);
+    this.center = [-100,-100];
+  }
+}
+class S3Card extends Card{
+  constructor(lineEndings){
+    super(3,lineEndings)
+  }
+}
+class S4Card extends Card{
+  constructor(lineEndings){
+    super(4,lineEndings);
+  }
+}
+
+function generatePermutation(numberOfLetters){
+  //list of integers 0,..,n-1
+  var orderedList = [];
+  for(var i=0;i<numberOfLetters;i++){
+    orderedList.push(i);
+  }
+  //Randomize the order of the list
+  var randomList = [];
+  for(var i=numberOfLetters;i>0;i--){
+    nextRandom = Math.floor(i*Math.random());
+    randomList.push(orderedList[nextRandom]);
+    orderedList.splice(nextRandom,1)
+  }
+  return randomList;
+}
+function deal(){
+  for(var i=0;i<inactiveSlots.length;i++){
+    if(inactiveSlots[i]==null){
+      newPermutation = generatePermutation(yPositions-1);
+      inactiveSlots[i] = new Card(yPositions - 1,newPermutation);
+      inactiveSlots[i].moveTo(inactivePositions[i]);
+      inactiveSlots[i].offPosition = i;
+      cardList[i] = inactiveSlots[i];
+    }
+  }
+}
---
+++ newCase/script.js
@@ -0,0 +1,115 @@
+var rainbow = new Rainbow(); //use this to generate colors programatically
+
+var elem = document.getElementById('draw-shapes');
+var params = { fullscreen: true };
+var two = new Two(params).appendTo(elem);
+
+//Save the width and the height of the screen
+var screenWidth = screen.width;
+var screenHeight = screen.height;
+
+//How big is a hand?
+var numberOfSlots = 7;
+
+//how many y positions do we need?
+//for Sn, we need n+1
+var yPositions = 10;
+
+//use this to compute the card size:
+var cardWidth = 3*screenWidth/(4*numberOfSlots + 2);
+var cardHeight = 5*cardWidth/3;
+
+var animationQueue = [];  //keeps animations in order
+var cardList = []; //keeps track of cards on the table
+
+//Positions of things relative to the origin
+var yList = [];
+for(var i=0;i<yPositions;i++){
+  yList.push(-.4*cardHeight + .8*i*cardHeight/(yPositions-1));
+}
+var xList = [-cardWidth/2,-cardWidth/4,cardWidth/4,cardWidth/2];
+
+//Save the colors of each row as global variables. (top to bottom)
+var colors = [];
+for(var i=0;i<yPositions-1;i++){
+  j = Math.floor(100*i/(yPositions-1));
+  //console.log('0x'+rainbow.colourAt(j));
+  colors.push('#'+rainbow.colourAt(j));
+}
+
+
+//The following lists store positions of cards (globally!)
+var activeSlots = [];
+var inactiveSlots = [];
+for(var i=0;i<numberOfSlots;i++){
+  inactiveSlots.push(null);
+}
+
+//also store the active and inactive positions.  We add one to each list to help with some computations of lines and balls later
+var activeY = 0.2*screenHeight;
+var inactiveY = 0.6*screenHeight;
+
+var activePositions = [];
+var inactivePositions = [];
+for(var i=0;i<numberOfSlots+1;i++){
+  nextX = cardWidth*(1+4*i/3);
+  activePositions.push([nextX,activeY]);
+  inactivePositions.push([nextX,inactiveY]);
+}
+
+deal();
+var powerBalls = makeBalls();
+var powerLines = makeLines();
+for (var i=0;i<yPositions-1;i++){
+  console.log(colors[i]);
+  powerBalls[0][i].fill=colors[i];
+  animationQueue.push([growBalls,0]);
+}
+
+function on_mouse_down(event){
+    if(animationQueue.length==0){
+    coords = click_to_plane_coords( event );
+    x = coords[0];
+    y = coords[1];
+
+    //first we have to save all the centers before we move things around
+    centerList = []
+    for (i = 0;i<cardList.length;i++){
+      centerList.push([cardList[i].center[0],cardList[i].center[1]]);
+    }
+    for (i=0;i<cardList.length;i++){
+      if(-1*cardWidth/2<x-centerList[i][0] && x-centerList[i][0]<cardWidth/2&&-1*cardHeight/2<y-centerList[i][1] && y-centerList[i][1]<cardHeight/2){
+        if(cardList[i].onClick()){
+          cleanUpActive(cardList[i].onPosition);
+        }
+      }
+    }
+  }
+}
+function click_to_plane_coords( event ) {
+	let rect = event.target.getBoundingClientRect();
+	let x = event.clientX;
+	let y = event.clientY;
+
+
+	return [ x, y ];
+}
+elem.addEventListener( "mousedown", on_mouse_down );
+
+//Let's put the animation stuff in here
+function animate(){
+  if(animationQueue.length > 0){
+    animationQueue[0][0]();
+  }
+  else{
+    checkWinCondition();
+  }
+  two.update();
+  requestAnimationFrame(animate);
+}
+requestAnimationFrame(animate);
+
+/*TO DO:
+  -We have an animation queue, but there is some lag with removing cards, might be worth streamlining things
+  -It might be worth working out a different animation scheme at some point.
+*/
